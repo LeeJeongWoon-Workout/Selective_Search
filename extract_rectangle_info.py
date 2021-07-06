16108c27aacc70739c3f714ec61b762912b6278c
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-07-06 21:11:08 +0900

    Create extract_rectangle_info.py

---
+++ extract_rectangle_info.py
@@ -0,0 +1,9 @@
+cand_rects = [cand['rect'] for cand in regions]
+print(cand_rects)
+
+'''--------------------------------------------
+rectangle 꼭짓점 정보만을 추철한다.
+(a,b,c,d)
+a,b 좌상단 좌표
+c:너비 d:높이
+'''
