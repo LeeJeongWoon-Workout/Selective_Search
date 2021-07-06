4a98df399a31d7992103823f16c60555c4ace997
Antarctica Citizen <75155964+LeeJeongWoon-Workout@users.noreply.github.com>
2021-07-06 20:40:36 +0900

    Update library.py

--- library.py
+++ library.py
@@ -1,3 +1,9 @@
 !pip install selectivesearch
 !mkdir /content/data
 !wget -O /content/data/audrey01.jpg https://raw.githubusercontent.com/chulminkw/DLCV/master/data/image/audrey01.jpg
+
+import selectivesearch
+import cv2
+import matplotlib.pyplot as plt
+import os
+%matplotlib inline
